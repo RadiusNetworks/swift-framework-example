13af82872f6ede97e19dc939b7bc66da3bf4859a
Michael Harper <mharper@standalonecode.com>
2014-10-07 11:20:11 -0700

    Initial Commit

---
+++ SimpleFramework/SimpleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SimpleFramework.h
+//  SimpleFramework
+//
+//  Created by Michael Harper on 10/7/14.
+//  Copyright (c) 2014 Radius Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleFramework.
+FOUNDATION_EXPORT double SimpleFrameworkVersionNumber;
+
+//! Project version string for SimpleFramework.
+FOUNDATION_EXPORT const unsigned char SimpleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleFramework/PublicHeader.h>
+
+
